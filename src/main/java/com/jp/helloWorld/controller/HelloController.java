9aad899e1deb6ede0d2f7374a78d0ce49c0edc4d
Angel Hita <angel.hita@talentech.com>
2023-07-02 18:03:02 +0200

    first commit

---
+++ src/main/java/com/jp/helloWorld/controller/HelloController.java
@@ -0,0 +1,71 @@
+package com.jp.helloWorld.controller;
+
+import com.jp.helloWorld.service.FileUploadService;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.text.MessageFormat;
+
+@RestController
+public class HelloController {
+
+	@Autowired
+	FileUploadService fileUploadService;
+
+	String header = "{\"alg\":\"RS256\",\"typ\":\"JWT\"}";
+	String claimTemplate = "'{'\"iss\": \"{0}\", \"sub\": \"{1}\", \"aud\": \"{2}\", \"exp\": \"{3}\"'}'";
+
+	@GetMapping("/")
+	public String getApplicationRoot(){
+		String data = "Service is up!";
+		return data;
+	}
+
+
+	@PostMapping
+	public String postApplication(
+			@RequestParam("file") MultipartFile file,
+			@RequestParam("data") String data,
+			@RequestParam("password") String password,
+			@RequestParam("alias") String alias
+	) throws IOException {
+		try {
+			fileUploadService.uploadFile(file);
+			String dataWithTime = data + "," + Long.toString(System.currentTimeMillis() / 1000L + 300L);
+			String[] claimArray = dataWithTime.split(",");
+			StringBuffer token = new StringBuffer();
+			token.append(Base64.encodeBase64URLSafeString(header.getBytes("UTF-8")));
+			token.append(".");
+			MessageFormat claims = new MessageFormat(claimTemplate);
+			String payload = claims.format(claimArray);
+			token.append(Base64.encodeBase64URLSafeString(payload.getBytes("UTF-8")));
+			KeyStore keystore = KeyStore.getInstance("JKS");
+			keystore.load(new FileInputStream("src/JWTkeystore.jks"), password.toCharArray());
+			PrivateKey privateKey = (PrivateKey)keystore.getKey(alias, password.toCharArray());
+			Signature signature = Signature.getInstance("SHA256withRSA");
+			signature.initSign(privateKey);
+			signature.update(token.toString().getBytes("UTF-8"));
+			String signedPayload = Base64.encodeBase64URLSafeString(signature.sign());
+			token.append(".");
+			token.append(signedPayload);
+			Path fileToDelete = Paths.get("src/JWTkeystore.jks");
+			Files.deleteIfExists(fileToDelete);
+
+			return token.toString();
+
+		} catch (Exception var11) {
+			var11.printStackTrace();
+			return "error";
+		}
+	}
+}
---
+++ src/main/java/com/jp/helloWorld/service/FileUploadService.java
@@ -0,0 +1,16 @@
+package com.jp.helloWorld.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class FileUploadService {
+    public void uploadFile(MultipartFile file) throws IOException {
+        String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+        file.transferTo(new File(dir + "/src/JWTkeystore.jks"));
+    }
+}
